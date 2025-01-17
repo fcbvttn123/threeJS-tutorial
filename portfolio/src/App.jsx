e6b4fa21146908c2001bdd3cd0fafafa8fea7e13
ngan <vutran@sheridancollege.ca>
2025-01-16 20:06:44 -0500

    Code with tutorial

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className="max-w-7xl mx-auto">
+      <h1 className="text-2xl text-white underline">Hello World</h1>
+    </main>
+  )
+}
+
+export default App
