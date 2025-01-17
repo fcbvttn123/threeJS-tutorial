9b3a04f55bd9b1da4a56a6580c8fede491d727e5
ngan <vutran@sheridancollege.ca>
2025-01-16 20:51:04 -0500

    Working on Nav Bar: Items

--- portfolio/src/sections/NavBar.jsx
+++ portfolio/src/sections/NavBar.jsx
@@ -1,5 +1,51 @@
-export function NavBar() {
+import { useState } from "react"
+import { navLinks } from "../constants"
+
+function NavItems() {
   return (
-    <header className="fixed top-0 left-0 right-0 z-50 bg-black/90"></header>
+    <ul className="nav-ul">
+      {navLinks.map(({ id, href, name }) => (
+        <li key={id} className="nav-li"></li>
+      ))}
+    </ul>
+  )
+}
+
+export function NavBar() {
+  const [isOpen, setIsOpen] = useState(false)
+  function toggleMenu() {
+    setIsOpen((prev) => !prev)
+  }
+  return (
+    // Always cover the whole screen width
+    <header className="fixed top-0 left-0 right-0 z-50 bg-black/90">
+      {/* Horizontally centered container */}
+      <div className="max-w-7xl mx-auto">
+        <div className="flex justify-between items-center py-5 mx-auto c-space">
+          {/* Child 1: Logo */}
+          <a
+            href="/"
+            className="text-neutral-400 font-bold text-xl hover:text-white transition-colors"
+          >
+            David Vu
+          </a>
+          {/* Child 2: Menu Icon */}
+          <button
+            onClick={toggleMenu}
+            className="text-neutral-400 hover:text-white focus:outline-none flex sm:hidden"
+          >
+            <img
+              src={isOpen ? "/assets/close.svg" : "/assets/menu.svg"}
+              alt="toggle"
+              className="w-6 h-6"
+            />
+          </button>
+          {/* Child 3: Navigation Items */}
+          <nav className="hidden sm:flex">
+            <NavItems />
+          </nav>
+        </div>
+      </div>
+    </header>
   )
 }
