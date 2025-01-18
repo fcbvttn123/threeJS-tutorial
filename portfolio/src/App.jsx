c644880a7f47115f34e3a8403ae63bf53ac849c7
ngan <vutran@sheridancollege.ca>
2025-01-17 19:12:27 -0500

    Create new component for Hero section

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,9 +1,11 @@
+import { Hero } from "./sections/Hero"
 import { NavBar } from "./sections/NavBar"
 
 function App() {
   return (
     <main className="max-w-7xl mx-auto">
       <NavBar />
+      <Hero />
     </main>
   )
 }
---
+++ portfolio/src/sections/Hero.jsx
@@ -0,0 +1,7 @@
+export function Hero() {
+  return (
+    <div>
+      <h1 className="text-white mt-20">Hero Section</h1>
+    </div>
+  )
+}
