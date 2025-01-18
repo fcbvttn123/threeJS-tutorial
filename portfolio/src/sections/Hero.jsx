87051933473fe6ffd01f28a061be37e9e3d79f81
ngan <vutran@sheridancollege.ca>
2025-01-17 19:22:14 -0500

    Basic setup for Hero section before moving to three js

--- portfolio/src/sections/Hero.jsx
+++ portfolio/src/sections/Hero.jsx
@@ -1,7 +1,13 @@
 export function Hero() {
   return (
-    <div>
-      <h1 className="text-white mt-20">Hero Section</h1>
-    </div>
+    <section className="min-h-screen w-full flex flex-col relative">
+      <div className="w-full mx-auto flex flex-col mt-20 c-space gap-3 sm:mt-36">
+        <p className="text-2xl sm:text-3xl font-medium text-white text-center font-generalsans">
+          Hi, I am David <span className="waving-hand">👋</span>
+        </p>
+        <p className="hero_tag text-gray_gradient">Full-stack Developer</p>
+      </div>
+      <div className="w-full h-full absolute inset-0"></div>
+    </section>
   )
 }
