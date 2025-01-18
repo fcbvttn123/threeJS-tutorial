2e5f316e76286fc4e61e1e7287ddb243710c8b30
ngan <vutran@sheridancollege.ca>
2025-01-17 21:24:30 -0500

    Working on rendering HackerRoom

--- portfolio/src/sections/Hero.jsx
+++ portfolio/src/sections/Hero.jsx
@@ -1,4 +1,18 @@
+import { PerspectiveCamera } from "@react-three/drei"
+import { Canvas } from "@react-three/fiber"
+import { HackerRoom } from "../components/HackerRoom"
+import { Suspense } from "react"
+import { CanvasLoader } from "../components/CanvasLoader"
+import { Leva, useControls } from "leva"
+
 export function Hero() {
+  //   const x = useControls("HackerRoom", {
+  //     rotationX: {
+  //       value: 2.5,
+  //       min: -10,
+  //       max: 10,
+  //     },
+  //   })
   return (
     <section className="min-h-screen w-full flex flex-col relative">
       <div className="w-full mx-auto flex flex-col mt-20 c-space gap-3 sm:mt-36">
@@ -8,7 +22,21 @@ export function Hero() {
         <p className="hero_tag text-gray_gradient">Full-stack Developer</p>
       </div>
       {/* npm i three @react-three/fiber @react-three/drei react-responsive leva */}
-      <div className="w-full h-full absolute inset-0"></div>
+      <div className="w-full h-full absolute inset-0">
+        <Canvas>
+          <Suspense fallback={<CanvasLoader />}>
+            <PerspectiveCamera makeDefault position={[0, 0, 30]} />
+            <HackerRoom
+              scale={0.06}
+              position={[0, 0, 0]}
+              rotation={[0, 280, 0]}
+            />
+            {/* <Leva /> */}
+            <ambientLight intensity={1} />
+            <directionalLight position={[10, 10, 10]} intensity={0.5} />
+          </Suspense>
+        </Canvas>
+      </div>
     </section>
   )
 }
