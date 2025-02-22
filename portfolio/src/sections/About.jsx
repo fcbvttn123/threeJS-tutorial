c92a0f848fab6d1cd075a56d254fe8d5b16576d3
ngan <vutran@sheridancollege.ca>
2025-02-22 03:54:36 -0500

    About section finished

--- portfolio/src/sections/About.jsx
+++ portfolio/src/sections/About.jsx
@@ -1,7 +1,16 @@
 import Globe from "react-globe.gl"
 import { Button } from "../components/Button.jsx"
+import { useState } from "react"
 
 export function About() {
+  const [hasCopied, setHasCopied] = useState(false)
+  function handleCopy() {
+    navigator.clipboard.writeText("davidvu316@gmail.com")
+    setHasCopied(true)
+    setTimeout(() => {
+      setHasCopied(false)
+    }, 2000)
+  }
   return (
     <section className="c-space my-20">
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 xl:grid-rows-6 gap-5 h-full">
@@ -61,6 +70,44 @@ export function About() {
                 worldwide.
               </p>
               <Button name="Contact Me" isBeam containerClass="w-full mt-10" />
+            </div>
+          </div>
+        </div>
+        <div className="xl:col-span-2 xl:row-span-3">
+          <div className="grid-container">
+            <img
+              src="assets/grid3.png"
+              alt="grid-3"
+              className="w-full sm:h-[266px] h-fit object-contain"
+            />
+            <div>
+              <p className="grid-headtext">My Passion for Coding</p>
+              <p className="grid-subtext">
+                I love solving problems and building things through code.
+                Programming isn&apos;t just my profession—it&apos;s my passion.
+                I enjoy exploring new technologies, and enhancing my skills.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="xl:col-span-1 xl:row-span-2">
+          <div className="grid-container">
+            <img
+              src="assets/grid4.png"
+              alt="grid-4"
+              className="w-full sm:h-[276px] md:h-[126px] h-fit object-cover sm:object-top"
+            />
+            <div className="space-y-2">
+              <p className="grid-subtext text-center">Contact me</p>
+              <div className="copy-container" onClick={handleCopy}>
+                <img
+                  src={hasCopied ? "assets/tick.svg" : "assets/copy.svg"}
+                  alt="copy"
+                />
+                <p className="lg:text-2xl md:text-xl font-medium text-gray_gradient text-white">
+                  davidvu316@gmail.com
+                </p>
+              </div>
             </div>
           </div>
         </div>
