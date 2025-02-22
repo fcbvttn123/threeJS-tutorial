60e1560bfab12c6a001bfe5d1ee415adcc1ae562
ngan <vutran@sheridancollege.ca>
2025-02-22 05:33:23 -0500

    Add 3D Model (DemoComputer) to the right side of the Project Card

--- portfolio/src/sections/Projects.jsx
+++ portfolio/src/sections/Projects.jsx
@@ -1,8 +1,9 @@
 import { Suspense, useState } from "react"
 import { myProjects } from "../constants"
 import { Canvas } from "@react-three/fiber"
-import { Center } from "@react-three/drei"
+import { Center, OrbitControls } from "@react-three/drei"
 import { CanvasLoader } from "../components/CanvasLoader"
+import { DemoComputer } from "../components/DemoComputer"
 
 const projectCount = myProjects.length
 
@@ -94,11 +95,14 @@ export function Projects() {
         </div>
         <div className="border border-black-300 bg-black-200 rounded-lg h-96 md:h-full">
           <Canvas>
-            <ambientLight intensity={1} />
+            <ambientLight intensity={Math.PI} />
             <directionalLight position={[10, 10, 5]} />
+            <OrbitControls enableZoom enablePan={false} enableRotate />
             <Center>
               <Suspense fallback={<CanvasLoader />}>
-                <group></group>
+                <group scale={2} position={[0, -3, 0]} rotation={[0, -0.1, 0]}>
+                  <DemoComputer />
+                </group>
               </Suspense>
             </Center>
           </Canvas>
